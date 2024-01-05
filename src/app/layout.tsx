dd531e3282662acba5769d58d0808542fb2deb64
Judicaël Akogbekan <judicael.akogbekan@axazara.io>
2024-01-05 12:24:21 +0100

    customers view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,17 +33,3 @@ export default function RootLayout({
   )
 }
 
-
-// import { ReactNode } from 'react';
-// import '@/css/tailwind.css';
-// import '@/css/custom.css';
-
-// type Props = {
-//   children: ReactNode;
-// };
-
-// // Since we have a `not-found.tsx` page on the root, a layout file
-// // is required, even if it's just passing children through.
-// export default function RootLayout({ children }: Props) {
-//   return children;
-// }
